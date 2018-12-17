e257cb3de64010bf49ed5da341d33995e6c7d6d0
James <james@fofo.tw>
2018-12-17 13:04:35 +0800

    ladder 篩選修補

--- application/models/Mod_voice_part_info.php
+++ application/models/Mod_voice_part_info.php
@@ -7,18 +7,23 @@ class Mod_voice_part_info extends CI_Model
     public function import($datas)
     {
         // 先清除當年資料
-        $this->db->where('year', $this->session->userdata('year'))->truncate('voice_area_main');
+        $this->db->where('year', $this->session->userdata('year'));
+        $this->db->where('ladder', $this->session->userdata('ladder'));
+        $this->db->delete('voice_area_main');
         $this->db->insert_batch('voice_area_main', $datas);
     }
 
     public function year_get_list()
     {
-        return $this->db->where('year', $this->session->userdata('year'))->get('voice_area_main')->result_array();
+        $this->db->where('year', $this->session->userdata('year'));
+        $this->db->where('ladder', $this->session->userdata('ladder'));
+        return $this->db->get('voice_area_main')->result_array();
     }
 
     public function get_list($part = '')
     {
         $this->db->where('year', $this->session->userdata('year'));
+        $this->db->where('ladder', $this->session->userdata('ladder'));
         if ($part != '') {
             $this->db->where('part', $part);
         }
@@ -48,15 +53,6 @@ class Mod_voice_part_info extends CI_Model
         $this->db->where('field<=', $data['end']);
         $res = $this->db->get('voice_area_main')->result_array();
        
-        // for ($i=0; $i < count($res); $i++) { 
-            
-        //     $arr[] = array(
-        //         'sn'=>$res[$i]['sn'],
-        //         'floor'=>$data['floor'],
-                
-        //     );
-        // }
-        // $this->db->update_batch('voice_area_main', $arr, 'sn');
         $this->db->update('voice_area_main',$data);
         return true;
     }    
