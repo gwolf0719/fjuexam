d0ddcd25f62d3bb9935eec9c3a1eaaa5b001f65d
harris70069 <harris70069h@gmail.com>
2019-03-04 12:10:42 +0800

    匯入只刪除匯入年

--- application/models/Mod_voice_area.php
+++ application/models/Mod_voice_area.php
@@ -43,7 +43,9 @@ class Mod_voice_area extends CI_Model {
 
     // 多筆輸入
     function insert_batch($datas){
-        $this->db->where('year', $this->session->userdata('year'))->truncate('voice_area_main');
+
+        $this->db->delete('year', $this->session->userdata('year'));
+        $this->db->where('year', $this->session->userdata('year'));
         $this->db->insert_batch('voice_area_main', $datas);
     }
   
