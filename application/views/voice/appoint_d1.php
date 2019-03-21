7807da2a111b096905368f5cca169702ebb33ab4
harris70069 <harris70069h@gmail.com>
2019-03-21 11:39:42 +0800

    D1 指派可以重覆

--- application/views/voice/appoint_d1.php
+++ application/views/voice/appoint_d1.php
@@ -213,6 +213,17 @@
                     $("#supervisor_1").val(arr[1]);
                     $("#trial_staff_code_1").val(arr[0]);
                     $('#exampleModal1').modal('hide');
+
+                    
+                    $s1=$("#supervisor_1").val();
+                    $s2=$("#supervisor_2").val();
+          
+                    if($s1==$s2){
+                        console.log('aspdskafas');
+                        $("#supervisor_1").val('');
+                        $("#trial_staff_code_1").val('');
+                        alert('重複選取人員');
+                    }
                 }
             })
         })
@@ -224,6 +235,16 @@
                     $("#supervisor_2").val(arr[1]);
                     $("#trial_staff_code_2").val(arr[0]);
                     $('#exampleModal2').modal('hide');
+
+                    $s1=$("#supervisor_1").val();
+                    $s2=$("#supervisor_2").val();
+          
+                    if($s1==$s2){
+                        console.log('aspdskafas');
+                        $("#supervisor_2").val('');
+                        $("#trial_staff_code_2").val('');
+                        alert('重複選取人員');
+                    }
                 }
             })
         })
