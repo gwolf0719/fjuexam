7f85afd029a4d7afc3ca33da1abb9d43680c2ce8
harris70069 <harris70069h@gmail.com>
2019-03-22 14:42:39 +0800

    A2 過濾梯次移除

--- application/models/Mod_school_unit.php
+++ application/models/Mod_school_unit.php
@@ -7,7 +7,7 @@ class Mod_school_unit extends CI_Model
     public function import($datas)
     {
         // 先清除當年資料
-        $this->db->where('year', $this->session->userdata('year'))->where('ladder', $this->session->userdata('ladder'))->delete('school_unit');
+        $this->db->where('year', $this->session->userdata('year'))->delete('school_unit');
         $this->db->insert_batch('school_unit', $datas);
     }
 
