44d5de0f619883b7f30416d5b7850a8380eb4806
harris70069 <harris70069h@gmail.com>
2019-03-26 18:08:55 +0800

    321人數問題

--- application/views/voice/form_e_3_2_1.php
+++ application/views/voice/form_e_3_2_1.php
@@ -61,6 +61,6 @@
   
     <?php endforeach; ?>
     <tr>
-        <td style="text-align:left;font-size:16px;">共計：<?=count($part)*2+$patrol_count?>人</td>
+        <td style="text-align:left;font-size:16px;">共計：<?=$trial_count*2+$patrol_count?>人</td>
     </tr>
 </table>
