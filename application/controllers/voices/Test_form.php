8a03f4248d8612dda48a695a2d97c5eb7efdcc60
harris70069 <harris70069h@gmail.com>
2019-03-26 10:59:46 +0800

    open pdf

--- application/controllers/voices/Test_form.php
+++ application/controllers/voices/Test_form.php
@@ -73,24 +73,24 @@ class Test_form extends CI_Controller
             'list' => $this->mod_voice_job_list->get_all_assign_member_list(),
         );
         // print_r($data);
-        echo $view =  $this->load->view('voice/form_e1_2', $data, true);
-        // if (!is_dir('./html/')) {
-        //     mkdir('./html/');
-        // } else {
-        //     $path = 'form_e1_2.html';
-        //     $fp = fopen('./html/'.$path,'w');//建檔
-        //     fwrite($fp,$view);
-        //     fclose($fp);//關閉開啟的檔案
-        //     // copy($path, './html/'.$path);
+        $view =  $this->load->view('voice/form_e1_2', $data, true);
+        if (!is_dir('./html/')) {
+            mkdir('./html/');
+        } else {
+            $path = 'form_e1_2.html';
+            $fp = fopen('./html/'.$path,'w');//建檔
+            fwrite($fp,$view);
+            fclose($fp);//關閉開啟的檔案
+            // copy($path, './html/'.$path);
 
-        // }
+        }
 
-        // if (!is_dir('./pdf/')) {
-        //     mkdir('./pdf/');
-        // } else {
-        //     exec('wkhtmltopdf --lowquality --enable-forms http://uat.fofo.tw/fjuexam/html/form_e1_2.html  ./pdf/form_e1_2.pdf');
-        // }
-        // echo '<script>location.href="http://uat.fofo.tw/fjuexam/pdf/form_e1_2.pdf"</script>';
+        if (!is_dir('./pdf/')) {
+            mkdir('./pdf/');
+        } else {
+            exec('wkhtmltopdf --lowquality --enable-forms http://uat.fofo.tw/fjuexam/html/form_e1_2.html  ./pdf/form_e1_2.pdf');
+        }
+        echo '<script>location.href="http://uat.fofo.tw/fjuexam/pdf/form_e1_2.pdf"</script>';
     }
 
     /**
