99aaf064f146e7570682143ffed2c472bf54bce2
harris70069 <harris70069h@gmail.com>
2019-03-26 18:33:19 +0800

    人數

--- application/views/voice/form_e_3_2_2.php
+++ application/views/voice/form_e_3_2_2.php
@@ -68,6 +68,6 @@
   
     <?php endforeach; ?>
     <tr>
-        <td style="text-align:left;font-size:16px;">共計：<?=$patrol_count?>人</td>
+        <td style="text-align:left;font-size:16px;">共計：<?=$trial_count*2+$patrol_count?>人</td>
     </tr>
 </table>
