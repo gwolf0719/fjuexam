7d6aca855516bfd75cb60801f05969969edfee42
harris70069 <harris70069h@gmail.com>
2019-04-01 11:44:17 +0800

    3-3

--- application/views/voice/form_e_3_3.php
+++ application/views/voice/form_e_3_3.php
@@ -40,34 +40,34 @@
     </tr>
 
     <tr>
-        <th class="bb" style="background:#FFE4E7;font-size:20px;">地點</th>
+        <th class="bb" style="background:#FFE4E7;font-size:21px;">地點</th>
         <th  class="bb" ><?=$data_list[$i]['area_name']?></th>
-        <th  class="bb" style="font-size:20px;" rowspan="2" style="background:#FFE4E7;font-size:20px;">起訖座號</th>
-        <th  class="bb" style="font-size:20px;" rowspan="2" colspan='2'><?=$data_list[$i]['start']?>-<?=$data_list[$i]['end']?></th>
-        <th  class="bb" style="background:#FFE4E7;font-size:20px;">本分區人數</th>
-        <th  class="bb" style="background:#FFE4E7;font-size:20px;">試場數</th>
+        <th  class="bb" style="font-size:21px;" rowspan="2" style="background:#FFE4E7;font-size:20px;">起訖座號</th>
+        <th  class="bb" style="font-size:21px;" rowspan="2" colspan='2'><?=$data_list[$i]['start']?>-<?=$data_list[$i]['end']?></th>
+        <th  class="bb" style="background:#FFE4E7;font-size:21px;">本分區人數</th>
+        <th  class="bb" style="background:#FFE4E7;font-size:21px;">試場數</th>
     </tr>
     <tr>
-        <th style="background:#FFE4E7;font-size:20px;"  class="bb" >起訖試場</th>
-        <th  class="bb" style="font-size:20px;"><?=$data_list[$i]['start_field']?>-<?=$data_list[$i]['end_field']?></th>
+        <th style="background:#FFE4E7;font-size:21px;"  class="bb" >起訖試場</th>
+        <th  class="bb" style="font-size:21px;"><?=$data_list[$i]['start_field']?>-<?=$data_list[$i]['end_field']?></th>
        
-        <td  class="bb" style="font-size:20px;"><?=$data_list[$i]['part_man_count']?></td>
-        <th  class="bb" style="font-size:20px;"><?=$data_list[$i]['field_count']?></th>
+        <td  class="bb" style="font-size:21px;"><?=$data_list[$i]['part_man_count']?></td>
+        <th  class="bb" style="font-size:21px;"><?=$data_list[$i]['field_count']?></th>
     </tr>
 
     <tr style="background:#FFE4E7">
-        <th class="bb" style="font-size:20px;">試場編號</th>
-        <th class="bb" style="font-size:20px;" colspan='2'>座位起訖號碼</th>
-        <th class="bb" style="font-size:20px;">考生人數</th>
-        <th class="bb" style="font-size:20px;" colspan='3'>試場大樓</th>
+        <th class="bb" style="font-size:21px;">試場編號</th>
+        <th class="bb" style="font-size:21px;" colspan='2'>座位起訖號碼</th>
+        <th class="bb" style="font-size:21px;">考生人數</th>
+        <th class="bb" style="font-size:21px;" colspan='3'>試場大樓</th>
     </tr>
 
     <?php foreach($data_list[$i]['field'] as $k2=>$v2):?>
     <tr>
-        <td class="bb" style="font-size:20px;font-weight:bold;"><?=$v2['field']?></td>
-        <td class="bb" style="font-size:20px;" colspan='2'><?=$v2['start']?>  <b style="font-size:20px;"> ~ </b> <?=$v2['end']?></td>
-        <td class="bb" style="font-size:20px;"><?=$v2['count_num']?></td>
-        <td class="bb" style="font-size:20px;" colspan='3'><?=$v2['floor']?></td>
+        <td class="bb" style="font-size:21px;font-weight:bold;"><?=$v2['field']?></td>
+        <td class="bb" style="font-size:21px;" colspan='2'><?=$v2['start']?>  <b style="font-size:20px;"> ~ </b> <?=$v2['end']?></td>
+        <td class="bb" style="font-size:21px;"><?=$v2['count_num']?></td>
+        <td class="bb" style="font-size:21px;" colspan='3'><?=$v2['floor']?></td>
     </tr>
     <?php endforeach;?>
 
