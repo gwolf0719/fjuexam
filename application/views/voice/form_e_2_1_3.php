f66037d79dbd22a4b031fdadd6b6a6e08e385cfe
harris70069 <harris70069h@gmail.com>
2019-04-01 15:26:35 +0800

    2-1

--- application/views/voice/form_e_2_1_3.php
+++ application/views/voice/form_e_2_1_3.php
@@ -36,7 +36,7 @@
 
 <table class="" id="" style="padding:5px 0px;;text-align:center;">
     <tr>
-        <td style="font-size:26px;lne-height:50px;" colspan="6"><?=$_SESSION['year']?>學年度英語聽力測驗<?=$_SESSION['ladder']?>考試新北一考區試務人員簽到表</td>
+        <td style="font-size:30px;lne-height:50px;" colspan="6"><?=$_SESSION['year']?>學年度英語聽力測驗<?=$_SESSION['ladder']?>考試新北一考區試務人員簽到表</td>
 
     <tr>
         <td colspan="2" style="font-size:22px;text-align:left;padding:20px 0px">分區：<?=$area?></td>
@@ -44,23 +44,23 @@
         <!-- <td colspan="1" style="font-size:22px;text-align:right;padding:20px 0px"></td> -->
     </tr>
     <tr>
-        <td style="border:1px solid #999" width="15%">職務</td>
-        <td style="border:1px solid #999" width="15%">姓名</td>
-        <td style="border:1px solid #999" width="15%">單位別</td>
-        <td style="border:1px solid #999" width="30%">簽名</td>
-        <td style="border:1px solid #999" width="15%">備註(工作分配)</td>
+        <td style="border:1px solid #999;font-size:20px;" width="15%">職務</td>
+        <td style="border:1px solid #999;font-size:20px;" width="15%">姓名</td>
+        <td style="border:1px solid #999;font-size:20px;" width="15%">單位別</td>
+        <td style="border:1px solid #999;font-size:20px;" width="30%">簽名</td>
+        <td style="border:1px solid #999;font-size:20px;" width="15%">備註(工作分配)</td>
     </tr>
     <?php foreach ($part as $kc => $vc): ?>
     <tr>
-        <td style="border:1px solid #999;height: 100px;"><?=$vc['job']?></td>
-        <td style="border:1px solid #999;height: 100px;"><?=$vc['name']?></td>
-        <td style="border:1px solid #999;height: 100px;"><?=$vc['member_unit']?></td>
-        <td style="border:1px solid #999;height: 100px;"></td>
-        <td style="border:1px solid #999;height: 100px;"><?=$vc['note']?></td>
+        <td style="border:1px solid #999;font-size:20px;height: 100px;"><?=$vc['job']?></td>
+        <td style="border:1px solid #999;font-size:20px;height: 100px;"><?=$vc['name']?></td>
+        <td style="border:1px solid #999;font-size:20px;height: 100px;"><?=$vc['member_unit']?></td>
+        <td style="border:1px solid #999;font-size:20px;height: 100px;"></td>
+        <td style="border:1px solid #999;font-size:20px;height: 100px;"><?=$vc['note']?></td>
     </tr>
     <?php endforeach; ?>
     <tr>
-        <td colspan="7" style="font-size:16px;text-align:left;">共計：<?=count($part)?>人</td>
+        <td colspan="7" style="font-size:20px;text-align:left;">共計：<?=count($part)?>人</td>
     </tr>
 </table>
 
