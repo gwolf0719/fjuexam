fd7d541426c90d4d2e89cd6401cedda8246cecb6
harris70069 <harris70069h@gmail.com>
2019-04-01 10:02:11 +0800

    報表字大小與欄位調整 E1-1 E1-2

--- application/views/voice/form_e1_1.php
+++ application/views/voice/form_e1_1.php
@@ -39,18 +39,18 @@
             <td colspan="3" style="font-size:28px;text-align:right;padding:20px 0px">印表日期：<?=date('Y/m/d')?></td>
         </tr>
         <tr>
-            <td colspan="1" style="border:1px solid #999;font-size:26px">序號</td>
-            <td colspan="1" style="border:1px solid #999;font-size:26px">部別</td>
-            <td colspan="2" style="border:1px solid #999;font-size:26px">代碼</td>
-            <td colspan="2" style="border:1px solid #999;font-size:26px">單位名稱</td>
+            <td colspan="1" style="border:1px solid #999;font-size:22px">序號</td>
+            <td colspan="1" style="border:1px solid #999;font-size:22px">部別</td>
+            <td colspan="2" style="border:1px solid #999;font-size:22px">代碼</td>
+            <td colspan="2" style="border:1px solid #999;font-size:22px">單位名稱</td>
         </tr>
     </thead>
     <?php foreach ($v as $kc => $vc): ?>
     <tr>
-        <td colspan="1" style="border:1px solid #999;font-size:26px"><?=($kc+1)?></td>
-        <td colspan="1" style="border:1px solid #999;font-size:26px"><?=$vc['department']?></td>
-        <td colspan="2" style="border:1px solid #999;font-size:26px"><?=$vc['code']?></td>
-        <td colspan="2" style="border:1px solid #999;font-size:26px"><?=$vc['company_name_02']?></td>
+        <td colspan="1" style="border:1px solid #999;font-size:22px"><?=($kc+1)?></td>
+        <td colspan="1" style="border:1px solid #999;font-size:22px"><?=$vc['department']?></td>
+        <td colspan="2" style="border:1px solid #999;font-size:22px"><?=$vc['code']?></td>
+        <td colspan="2" style="border:1px solid #999;font-size:22px"><?=$vc['company_name_02']?></td>
     </tr>
     <?php endforeach;?>
 </table>
--- application/views/voice/form_e1_2.php
+++ application/views/voice/form_e1_2.php
@@ -32,47 +32,32 @@
 <table class="" id="" style="padding:4px;text-align:center;">
     <thead>
         <tr>
-            <td colspan="13" style="font-size:26px;">請公假名單</td>
+            <td colspan="13" style="font-size:30px;">請公假名單</td>
         </tr>
         <tr>
-            <td colspan="13" style="text-align:right;font-size:22px;">印表日期：<?=(date("Y") - 1911).date("/m/d")?></td>
+            <td colspan="13" style="text-align:right;font-size:28px;">印表日期：<?=(date("Y") - 1911).date("/m/d")?></td>
         </tr>
         <tr style="background:#FFE4E7">
-            <th class="bb" colspan="1">序號</th>
-            <th class="bb" colspan="2">職員代碼</th>
-            <th class="bb" colspan="2">姓名</th>
-            <th class="bb" colspan="3">單位</th>
-            <th class="bb" colspan="2">職稱</th>
-            <th class="bb" colspan="3">執勤日期</th>
+            <th class="bb" colspan="1" style="font-size:28px;">序號</th>
+            <th class="bb" colspan="2" style="font-size:28px;">職員代碼</th>
+            <th class="bb" colspan="2" style="font-size:28px;">姓名</th>
+            <th class="bb" colspan="3" style="font-size:28px;">單位</th>
+            <th class="bb" colspan="2" style="font-size:28px;">職稱</th>
+            <th class="bb" colspan="3" style="font-size:28px;">執勤日期</th>
         </tr>
     </thead>
     <?php  $i=0?>
         <?php foreach ($list as $k => $v): ?>
         <tr>
-            <td class="bb" colspan="1">
-                <?=$i + 1?>
-            </td>
-            <td class="bb" colspan="2">
-                <?=$v['job_code']?>
-            </td>
-            <td class="bb" colspan="2">
-                <?=$v['name']; ?>
-            </td>
-            <td class="bb" colspan="3">
-                <?=$v['member_unit']; ?>
-            </td>
-            <td class="bb" colspan="2">
-                <?=$v['job_title']; ?>
-            </td>
-            <td class="bb" colspan="3">
+            <td class="bb" colspan="1" style="font-size:22px;"><?=$i + 1?></td>
+            <td class="bb" colspan="2" style="font-size:22px;"><?=$v['job_code']?></td>
+            <td class="bb" colspan="2" style="font-size:22px;"><?=$v['name']; ?></td>
+            <td class="bb" colspan="3" style="font-size:22px;"><?=$v['member_unit']; ?></td>
+            <td class="bb" colspan="2" style="font-size:22px;"><?=$v['job_title']; ?></td>
+            <td class="bb" colspan="3" style="font-size:22px;">
                 <?php 
-                
                 $date= explode(",",$v['do_date']);
                 echo $date[0];
-                
-                
-                
-                
                 ?>
                 
             </td>
