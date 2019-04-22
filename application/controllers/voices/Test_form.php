0f9dc9019a177c8ac3259e769a881a6bf1b63723
harris70069 <harris70069h@gmail.com>
2019-04-22 16:11:14 +0800

    6-1

--- application/controllers/voices/Test_form.php
+++ application/controllers/voices/Test_form.php
@@ -1795,46 +1795,46 @@ class Test_form extends CI_Controller
         $arr = array();
         $fields = array();
         $dataarray = $this->mod_voice_trial->e_6_1($part);
-        // $i = 0;
-        // foreach ($dataarray as $k => $v) {
+        $i = 0;
+        foreach ($dataarray as $k => $v) {
 
-        //     $key = array_search($v['field'], $fields);
-        //     if ($key === false) {
-        //         $fields[] = $v['field'];
-        //         $arr[$i] = $v;
-        //         $i++;
-        //     }
+            $key = array_search($v['field'], $fields);
+            if ($key === false) {
+                $fields[] = $v['field'];
+                $arr[$i] = $v;
+                $i++;
+            }
 
-        // }
+        }
 
 
-        // $data = array(
-        //     'part' => $arr,
-        //     'area' => $area,
-        //     'school' => $this->mod_voice_exam_area->year_school_name($part),
-        //     'salary' => $this->mod_voice_trial->get_all_salary_trial_total($part),
-        //     'count' => $this->mod_voice_trial->e_6_1_member_count($part)
-        // );
+        $data = array(
+            'part' => $arr,
+            'area' => $area,
+            'school' => $this->mod_voice_exam_area->year_school_name($part),
+            'salary' => $this->mod_voice_trial->get_all_salary_trial_total($part),
+            'count' => $this->mod_voice_trial->e_6_1_member_count($part)
+        );
     
 
-        // // $view = $this->load->view('voice/form_e_6_1', $data);
+        // $view = $this->load->view('voice/form_e_6_1', $data);
 
-        // echo $view = $this->load->view('voice/form_e_6_1', $data, true);
-        // if (!is_dir('./html/')) {
-        //     mkdir('./html/');
-        // } else {
-        //     $path = 'form_e_6_1.html';
-        //     $fp = fopen('./html/'.$path,'w');//建檔
-        //     fwrite($fp,$view);
-        //     fclose($fp);//關閉開啟的檔案
-        // }
+        echo $view = $this->load->view('voice/form_e_6_1', $data, true);
+        if (!is_dir('./html/')) {
+            mkdir('./html/');
+        } else {
+            $path = 'form_e_6_1.html';
+            $fp = fopen('./html/' . $path, 'w');//建檔
+            fwrite($fp, $view);
+            fclose($fp);//關閉開啟的檔案
+        }
 
-        // if (!is_dir('./pdf/')) {
-        //     mkdir('./pdf/');
-        // } else {
-        //     exec('wkhtmltopdf --lowquality --enable-forms http://uat.fofo.tw/fjuexam/html/form_e_6_1.html  ./pdf/form_e_6_1.pdf');
-        // }
-        // echo '<script>location.href="http://uat.fofo.tw/fjuexam/pdf/form_e_6_1.pdf"</script>';
+        if (!is_dir('./pdf/')) {
+            mkdir('./pdf/');
+        } else {
+            exec('wkhtmltopdf --lowquality --enable-forms http://uat.fofo.tw/fjuexam/html/form_e_6_1.html  ./pdf/form_e_6_1.pdf');
+        }
+        echo '<script>location.href="http://uat.fofo.tw/fjuexam/pdf/form_e_6_1.pdf"</script>';
     }
 
     public function form_e_6_2()
--- application/views/voice/form_e_6_1.php
+++ application/views/voice/form_e_6_1.php
@@ -1,30 +1,41 @@
 <style>
-    .bb {
-        border: 1px solid #999999;
-    }
-   table{
-        text-align: center;
-        border-spacing: 0px;
-        width:100%;
-        font-family: serif,cursive;
-    }
-    td{
-        font-size:16px;
-        padding:5px 0px;
-        font-family: serif,cursive;
-    }
-    * {
-        overflow: visible !important;
+.bb {
+    border: 1px solid #999999;
+}
 
-    }
-    table, tr, td, th, tbody, thead, tfoot {
-        page-break-before: always;
-        page-break-inside: avoid;
-    }
-    .W50{
-        width:50%;
-        float:left;
-    }
+table {
+    text-align: center;
+    border-spacing: 0px;
+    width: 100%;
+    font-family: serif, cursive;
+}
+
+td {
+    font-size: 16px;
+    padding: 5px 0px;
+    font-family: serif, cursive;
+}
+
+* {
+    overflow: visible !important;
+
+}
+
+table,
+tr,
+td,
+th,
+tbody,
+thead,
+tfoot {
+    page-break-before: always;
+    page-break-inside: avoid;
+}
+
+.W50 {
+    width: 50%;
+    float: left;
+}
 </style>
 
 
@@ -37,22 +48,23 @@
 <table class="" id="" style="text-align:center;">
     <thead>
         <tr>
-            <td colspan="7" style="font-size:26px;font-family: serif,cursive;"><?=$_SESSION['year']?>學年度英語聽力測驗<?=$_SESSION['ladder']?>考試新北一考區</td>
+            <td colspan="7" style="font-size:26px;font-family: serif,cursive;">
+                <?= $_SESSION['year'] ?>學年度英語聽力測驗<?= $_SESSION['ladder'] ?>考試新北一考區</td>
         </tr>
         <tr>
             <td colspan="7" style="font-size:22px;">監試人員印領清冊</td>
         </tr>
         <tr>
-            <td colspan="2" style="font-size:20px;padding: 20px 0px;"> 分區：<?=$area?></td>
-            <td colspan="3" style="font-size:20px;padding: 20px 0px;">考場：<?=$school['area_name']?></td>
-            <td colspan="2" style="font-size:20px;padding: 20px 0px;"> 印表日期：<?=date('Y/m/d')?></td>
+            <td colspan="2" style="font-size:20px;padding: 20px 0px;"> 分區：<?= $area ?></td>
+            <td colspan="3" style="font-size:20px;padding: 20px 0px;">考場：<?= $school['area_name'] ?></td>
+            <td colspan="2" style="font-size:20px;padding: 20px 0px;"> 印表日期：<?= date('Y/m/d') ?></td>
         </tr>
         <tr>
             <td rowspan="2" class="bb" style="width:5%">試場</td>
-            <td  nowrap="nowrap" colspan="1" class="bb">監試人員(1)</td>
+            <td nowrap="nowrap" colspan="1" class="bb">監試人員(1)</td>
             <td rowspan="2" class="bb">監考費</td>
             <td rowspan="2" class="bb" style="width:145">簽名或蓋章</td>
-            <td  nowrap="nowrap" colspan="1" class="bb">監試人員(2)</td>
+            <td nowrap="nowrap" colspan="1" class="bb">監試人員(2)</td>
             <td rowspan="2" class="bb">監考費</td>
             <td rowspan="2" class="bb" style="width:145">簽名或蓋章</td>
         </tr>
@@ -62,49 +74,50 @@
         </tr>
     </thead>
     <?php
-        $member_count = 0;
-        $money_count = 0;
+    $member_count = 0;
+    $money_count = 0;
     ?>
-    <?php foreach ($part as $k => $v): ?>
-        <?php 
-        if($v['supervisor_1'] != ""){
-            $member_count = $member_count+1;
-        }
+    <?php foreach ($part as $k => $v) : ?>
+    <?php 
+    if ($v['supervisor_1'] != "") {
+        $member_count = $member_count + 1;
+    }
 
 
 
-        if($v['supervisor_2'] != ""){
-            $member_count = $member_count+1;
-        }
+    if ($v['supervisor_2'] != "") {
+        $member_count = $member_count + 1;
+    }
 
 
 
 
 
-        $money_count = $money_count+$v['first_member_salary_section']+$v['second_member_salary_section'];
-        ?>
+    $money_count = $money_count + $v['first_member_section_salary_total'] + $v['second_member_section_salary_total'];
+    ?>
     <tr>
-        <td class="bb"  style="width:8%;font-size:18px;font-weight:bold;">
-            <?=trim($v['field'])?>
+        <td class="bb" style="width:8%;font-size:18px;font-weight:bold;">
+            <?= trim($v['field']) ?>
         </td>
-        <td class="bb" style="width:8%"><?=trim($v['supervisor_1'])?>
+        <td class="bb" style="width:8%"><?= trim($v['supervisor_1']) ?>
         </td>
-        <td class="bb" style="width:8%;font-size:18px;font-weight:bold;" >
-            <?=trim(number_format($v['first_member_salary_section']))?>
+        <td class="bb" style="width:8%;font-size:18px;font-weight:bold;">
+            <?= trim(number_format($v['first_member_section_salary_total'])) ?>
         </td>
-     
+
         <td class="bb" style="padding: 30px 0px;"></td>
-        <td class="bb" style="width:8%"><?=trim($v['supervisor_2'])?>
+        <td class="bb" style="width:8%"><?= trim($v['supervisor_2']) ?>
         </td>
-        <td class="bb" style="width:8%;font-size:18px;font-weight:bold;" >
-            <?=trim(number_format($v['second_member_salary_section']))?>
+        <td class="bb" style="width:8%;font-size:18px;font-weight:bold;">
+            <?= trim(number_format($v['second_member_section_salary_total'])) ?>
         </td>
-      
+
         <td class="bb" style="padding: 30px 0px;"></td>
     </tr>
     <?php endforeach; ?>
     <tr>
-        
-        <td colspan="11" style="text-align:left;font-size:18px;font-weight:bold">共計:<?=$member_count?>人 實發監考費：<?=number_format($money_count)?> = 總支出費用<?=number_format($money_count)?> </td>
+
+        <td colspan="11" style="text-align:left;font-size:18px;font-weight:bold">共計:<?= $member_count ?>人
+            實發監考費：<?= number_format($money_count) ?> = 總支出費用<?= number_format($money_count) ?> </td>
     </tr>
 </table>
