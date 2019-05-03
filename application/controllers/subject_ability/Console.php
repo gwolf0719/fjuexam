1737b3697a4786b2f0407b9bf87618ab50cb0ea5
harris70069 <harris70069h@gmail.com>
2019-05-03 11:03:21 +0800

    pdf

--- application/controllers/subject_ability/Console.php
+++ application/controllers/subject_ability/Console.php
@@ -1655,8 +1655,8 @@ class Console extends CI_Controller
         );
 
         if ($data['part'] != false) {
-            echo $view = $this->load->view('subject_ability/e_2_2', $data, true);
-            // $this->pdf->view_to_pdf($view, 'e_2_2');
+            $view = $this->load->view('subject_ability/e_2_2', $data, true);
+            $this->pdf->view_to_pdf($view, 'e_2_2');
         } else {
             return false;
         }
