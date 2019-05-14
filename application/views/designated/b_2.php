36d90ffda7e27196d01173404323a07fca8cb758
harris70069 <harris70069h@gmail.com>
2019-05-14 15:02:18 +0800

    指考B區圖片

--- application/views/designated/b_2.php
+++ application/views/designated/b_2.php
@@ -472,7 +472,7 @@ $(function() {
     </div>
 
     <div class="col-sm-8" style="text-align: center;">
-        <img src="assets/images/b1_title.png" alt="" style="width: 15%;">
+        <img src="assets/images/b2_title.png" alt="" style="width: 15%;">
     </div>
 
 </div>
